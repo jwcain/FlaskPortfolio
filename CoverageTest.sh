e21e2d5a54b03df656bb5f7082a4473eb421ed76
Justin Cain <bestagonstudio@gmail.com>
2023-08-06 02:12:08 -0400

    Modified coverage test script to report coverage then ask to generate html

--- CoverageTest.sh
+++ CoverageTest.sh
@@ -1,4 +1,13 @@
 coverage run -m pytest
+coverage report
+
+read -p "Generate HTML? [y/n]" yn
+
+case $yn in
+    y ) echo "Generating";;
+    * ) exit;;
+
+esac
 coverage html
 explorer '.\htmlcov\index.html'
-read
+exit
